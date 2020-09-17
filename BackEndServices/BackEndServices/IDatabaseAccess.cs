08548d0c2700c74fb1ddb7d8a55d2da7ddb3f98c
mart610k <martin-baage@hotmail.com>
2020-09-17 10:13:49 +0200

    Added a test libary and an interface

---
+++ BackEndServices/BackEndServices/IDatabaseAccess.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BackEndServices
+{
+    public interface IDatabaseAccess
+    {
+        List<T> LoadData<T>(string sql);
+
+        bool UpdateData(string sql);
+
+        bool SaveData(string sql);
+    }
+}
---
+++ BackEndServices/BackEndServicesTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BackEndServicesTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
