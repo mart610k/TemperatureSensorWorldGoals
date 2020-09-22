9b32f08b730af4cb9e6f31b603eaeddc78324df5
mart610k <martin-baage@hotmail.com>
2020-09-22 12:34:52 +0200

    Implemented a concrete method for getting data. Updated UML diagram and ER diagram

--- BackEndServices/BackEndServices/Database/MySQLDatabaseAccess.cs
+++ BackEndServices/BackEndServices/Database/MySQLDatabaseAccess.cs
@@ -3,6 +3,8 @@ using BackEndServices.Sensor;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data.MySqlClient;
+
 
 namespace BackEndServices.Database
 {
@@ -27,56 +29,217 @@ namespace BackEndServices.Database
             return string.Format("Server={0};Database={1};Uid={2};Pwd={3};",Host,DatabaseName,UserName,UserPassword);
         }
 
-
         public bool CreateRoom(IRoom room)
         {
-            throw new NotImplementedException();
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "INSERT INTO Sensor(ID,`Name`,Macaddress,IpAddress,`Description`) VALUE (UUID_TO_BIN(@UUID),@RoomName,UNHEX(@MacAddress),@IpAddress,@Description);";
+
+            command.Parameters.AddWithValue("@UUID", room.GUID.ToString());
+            command.Parameters.AddWithValue("@RoomName", room.Name);
+            command.Parameters.AddWithValue("@MacAddress", room.MACAddress);
+            command.Parameters.AddWithValue("@IpAddress", room.IPaddress);
+            command.Parameters.AddWithValue("@Description", room.Description);
+
+            conn.Open();
+
+            int result = command.ExecuteNonQuery();
+            
+            conn.Close();
+
+            if(result == 1)
+            {
+                return true;
+            }
+            return false;
+            
         }
 
-        public bool CreateSensorReading(string roomUUID, ISensorReading sensorReading)
+
+        public bool RegisterSensor(ISensor sensor)
         {
-            throw new NotImplementedException();
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "INSERT INTO SensorType(SensorName) VALUE (@SensorName);";
+            command.Parameters.AddWithValue("@SensorName", sensor.SensorName);
+
+            conn.Open();
+            int result = command.ExecuteNonQuery();
+            conn.Close();
+
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
         }
 
         public IRoom GetDetailedRoom(string roomuuid)
         {
-            throw new NotImplementedException();
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "SELECT BIN_TO_UUID(ID),Name,HEX(MacAddress),IpAddress,Description FROM Sensor WHERE ID = UUID_TO_BIN(@UUID);";
+
+            command.Parameters.AddWithValue("@UUID", roomuuid);
+
+            conn.Open();
+
+            MySqlDataReader reader = command.ExecuteReader();
+            IRoom room = null;
+            while (reader.HasRows)
+            {
+                reader.Read();
+                room = new Room.Room(reader.GetString("Name"), reader.GetString("BIN_TO_UUID(ID)"), reader.GetString("HEX(MacAddress)"), reader.GetString("IpAddress"), reader.GetString("Description"));
+            }
+
+            return room;
         }
 
         public ISimpleRoom[] GetRooms()
         {
-            throw new NotImplementedException();
-        }
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
 
-        public ISensorLimit GetSensorForLimitForRoom(string roomuuid, int sensorTypeID)
-        {
-            throw new NotImplementedException();
-        }
+            command.CommandText = "SELECT BIN_TO_UUID(ID),Name FROM Sensor;";
 
-        public ISensorReading[] GetSensorReadings(string roomuuid, int sensorTypeID, int count)
-        {
-            throw new NotImplementedException();
+            conn.Open();
+
+            MySqlDataReader reader = command.ExecuteReader();
+            List<ISimpleRoom> roomList = new List<ISimpleRoom>();
+
+            while (reader.HasRows)
+            {
+                reader.Read();
+                roomList.Add(new SimpleRoom(reader.GetString("Name"), reader.GetString("BIN_TO_UUID(ID)")));
+            }
+
+            reader.Close();
+            conn.Close();
+
+            return roomList.ToArray();
         }
 
         public ISensor[] GetSensors()
         {
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "SELECT ID,SensorName FROM SensorType;";
+
+            List<ISensor> sensors = new List<ISensor>();
+            conn.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.HasRows)
+            {
+                reader.Read();
+                sensors.Add(new Sensor.Sensor(reader.GetInt32("ID"), reader.GetString("SensorName")));
+
+            }
+            reader.Close();
+            conn.Close();
+
+            return sensors.ToArray();
+        }
+
+        public bool CreateSensorReading(string roomUUID, ISensorReading sensorReading)
+        {
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "INSERT INTO SensorReading(SensorID,SensorTypeID,TimeRead,ValueRead) VALUE (UUID_TO_BIN(@UUID),@SensorID,@DateTime,@ValueRead);";
+            command.Parameters.AddWithValue("@UUID", roomUUID);
+            command.Parameters.AddWithValue("@SensorID", sensorReading.SensorID);
+            command.Parameters.AddWithValue("@DateTime", sensorReading.TimeRead.ToString("yyyy-MM-dd H:mm:ss"));
+            command.Parameters.AddWithValue("@ValueRead", sensorReading.ValueRead);
+
+            conn.Open();
+            int result = command.ExecuteNonQuery();
+            conn.Close();
+
+            if(result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public ISensorReading[] GetSensorReadings(string roomuuid, int sensorTypeID, int count)
+        {
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+
+            MySqlCommand command = conn.CreateCommand();
+
+            command.CommandText = "SELECT " +
+                "SensorReading.TimeRead,SensorReading.ValueRead,SensorReading.SensorTypeID,SensorType.SensorName " +
+                "FROM SensorReading " +
+                "JOIN SensorType ON SensorType.ID = SensorReading.SensorTypeID " +
+                "WHERE SensorReading.SensorID = UUID_TO_BIN(@UUID) AND SensorReading.SensorTypeID = @SensorID " +
+                "ORDER BY SensorReading.TimeRead DESC Limit @Count;";
+
+            command.Parameters.AddWithValue("@UUID", roomuuid);
+            command.Parameters.AddWithValue("@SensorID", sensorTypeID);
+            command.Parameters.AddWithValue("@Count", count);
+            List<ISensorReading> sensorReadings = new List<ISensorReading>();
+            conn.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.HasRows)
+            {
+                reader.Read();
+                sensorReadings.Add(new SensorReading(reader.GetInt32("SensorReading.ValueRead"), reader.GetString("SensorType.SensorName"), reader.GetDateTime("SensorReading.TimeRead"), reader.GetFloat("SensorReading.ValueRead")));
+            }
+            reader.Close();
+
+            conn.Close();
+
+            return sensorReadings.ToArray();
+        }
+
+
+        public bool SetSensorLimitForRoom(string roomuuid, ISensorLimit sensorLimit)
+        {
+            MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            MySqlCommand command = conn.CreateCommand();
+
+
+            command.CommandText = "INSERT INTO CustomSensorLimit(SensorTypeID,SensorID,UpperLimit,LowerLimit) " +
+                "VALUE (@SensorTypeID,UUID_TO_BIN(@UUID),@UpperLimit,@LowerLimit);";
+
+            command.Parameters.AddWithValue("@SensorTypeID", sensorLimit.SensorID);
+            command.Parameters.AddWithValue("@UUID", roomuuid);
+            command.Parameters.AddWithValue("@UpperLimit", sensorLimit.UpperLimit);
+            command.Parameters.AddWithValue("@LowerLimit", sensorLimit.LowerLimit);
+
+            conn.Open();
+            int result = command.ExecuteNonQuery();
+            conn.Close();
+
+            if(result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public ISensorLimit GetSensorForLimitForRoom(string roomuuid, int sensorTypeID)
+        {
+            //MySqlConnection conn = new MySqlConnection(GetConnectionString());
+            //MySqlCommand command = conn.CreateCommand();
             throw new NotImplementedException();
         }
+
+        
+
 
         public ISensor[] GetSensorsForRoom(string roomuuid)
         {
             throw new NotImplementedException();
         }
 
-        public bool RegisterSensor(ISensor sensor)
-        {
-            throw new NotImplementedException();
-        }
 
-        public bool SetSensorLimitForRoom(string roomuuid, ISensorLimit sensorLimit)
-        {
-            throw new NotImplementedException();
-        }
 
         public bool UpdateRoom(IRoom room)
         {
