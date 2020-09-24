45214b8eb22362b06dccecf8bb739cc1d39a2f6b
mart610k <martin-baage@hotmail.com>
2020-09-24 09:37:33 +0200

    Ardiuno communicator baseline created

--- ArduinoCommunicator/ArduinoCommunicator/Program.cs
+++ ArduinoCommunicator/ArduinoCommunicator/Program.cs
@@ -6,42 +6,81 @@ using Newtonsoft.Json;
 using System.Threading;
 using BackEndServices.Database;
 using BackEndServices.Room;
+using BackEndServices.Sensor;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.IO;
 
 namespace ArduinoCommunicator
 {
     class Program
     {
-        static HttpClient client = new HttpClient();
+
+
+        public static CommunicatorActions communicator; 
+
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            SensorUpdater sensorUpdater = SensorUpdater.GetInstance();
+            sensorUpdater.StopAndJoinThreads();
+        }
+
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static Dictionary<string, string> ReadConfigFile()
+        {
+            Dictionary<string, string> config = new Dictionary<string, string>();
+
+            using (StreamReader str = new StreamReader(@"D:\Skole\H3\TemperatureSensorWorldGoalsOld\mysqlConnectionConfig.cfg"))
+            {
+                string configFile = str.ReadToEnd();
+                string[] tmp = configFile.Split(Environment.NewLine);
+
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    Console.WriteLine(tmp[i]);
+                    string[] values = tmp[i].Split("=");
+                    config.Add(values[0], values[1]);
+                }
+            }
+            return config;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-             CommunicatorActions communicator = new CommunicatorActions(new MySQLDatabaseAccess("127.0.0.1", "WorldGoals", "test", "rSFC68k0QY"));
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
 
-            ISimpleRoom[] rooms = communicator.GetRooms();
-            for (int i = 0; i < rooms.Length; i++)
+            Dictionary<string, string> config = ReadConfigFile();
+
+            communicator = new CommunicatorActions(new MySQLDatabaseAccess(config["host"], config["databasename"], config["username"], config["password"]));
+
+            List<IRoom> rooms = GetRooms(communicator, communicator.GetRooms());
+            
+            SensorUpdater updater = SensorUpdater.GetInstance();
+
+            for (int i = 0; i < rooms.Count; i++)
             {
-                Console.WriteLine(rooms[i].GUID);
+                updater.RegisterThread(rooms[i].GUID.ToString(),rooms[i].IPaddress, 5);
             }
+            Console.ReadKey();
 
-            while (true)
-            {
-                
-
-                GetData();
-
-                Thread.Sleep(1000);
-            }
+            updater.StopAndJoinThreads();
 
             Console.ReadKey();
         }
 
-        async static void GetData()
+        public static List<IRoom> GetRooms(ICommonDataMethods commonDataMethods, ISimpleRoom[] uuid)
         {
-            HttpResponseMessage response = await client.GetAsync("http://127.0.0.1:5000");
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
+            List<IRoom> rooms = new List<IRoom>();
 
-            Console.WriteLine(responseBody);
+            for (int i = 0; i < uuid.Length; i++)
+            {
+                rooms.Add(commonDataMethods.GetRoomDetailed(uuid[i].GUID.ToString()));
+            }
+            return rooms;
         }
     }
 }
---
+++ ArduinoCommunicator/ArduinoCommunicator/SensorUpdater.cs
@@ -0,0 +1,124 @@
+﻿using BackEndServices.Sensor;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+
+namespace ArduinoCommunicator
+{
+    class SensorUpdater
+    {
+        private List<Thread> threads;
+
+        static bool threadsShouldRun = true;
+
+        static object threadShouldRunLock = new object();
+
+        static SensorUpdater SensorUpdaterSingleton;
+
+        static object SensorUpdaterSingletonLock = new object();
+
+        public static SensorUpdater GetInstance()
+        {
+            lock (SensorUpdaterSingletonLock)
+            {
+                if (SensorUpdaterSingleton == null)
+                {
+                    SensorUpdaterSingleton = new SensorUpdater();
+                }
+                return SensorUpdaterSingleton;
+            }
+        }
+        private SensorUpdater()
+        {
+            threads = new List<Thread>();
+        }
+
+        public bool StopAndJoinThreads()
+        {
+            lock (threadShouldRunLock)
+            {
+                threadsShouldRun = false;
+            }
+
+            for (int i = 0; i < threads.Count; i++)
+            {
+                threads[i].Join();
+            }
+
+            threads = new List<Thread>();
+
+
+            return true;
+        }
+
+
+        public bool RegisterThread(string roomUUID, string host, int waitingTimeInSeconds)
+        {
+            Thread thread = new Thread(() => SensorGetter(roomUUID, host, waitingTimeInSeconds));
+            thread.Start();
+            threads.Add(thread);
+            return true;
+        }
+
+        private void SensorGetter(string roomUUID,string host, int waitingTimeInSeconds)
+        {
+            HttpClient client = new HttpClient();
+
+            client.Timeout = TimeSpan.FromSeconds(5);
+
+            while (threadsShouldRun)
+            {
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync("http://" + host).GetAwaiter().GetResult();
+
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    switch (response.StatusCode)
+                    {
+                        case System.Net.HttpStatusCode.OK:
+                            JObject jObject = JObject.Parse(responseBody);
+
+                            List<string> keys = jObject.Properties().Select(p => p.Name).ToList();
+                            for (int i = 0; i < keys.Count; i++)
+                            {
+                                JToken jToken = jObject.GetValue(keys[i]);
+
+                                if(jToken.Type == JTokenType.Float || jToken.Type == JTokenType.Integer)
+                                {
+                                    float valueRead = (float)jToken;
+                                    ISensor sensor = Program.communicator.GetSensorByName(keys[i]);
+                                    ISensorReading sensorReading = new SensorReading(sensor.SensorID, sensor.SensorName, DateTime.UtcNow, valueRead);
+
+                                    Program.communicator.CreateSensorReading(roomUUID, sensorReading);
+                                }
+                            }
+
+                           
+                            
+
+
+                            Console.WriteLine(jObject.Value<float>("temperature"));
+                            Console.WriteLine(jObject.Value<float>("humidity"));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    
+                }
+                Thread.Sleep(waitingTimeInSeconds * 1000);
+            }
+        
+            client.Dispose();
+
+            Program.PrintMessage("Awaiting to be joined");
+        }
+
+}
+}
