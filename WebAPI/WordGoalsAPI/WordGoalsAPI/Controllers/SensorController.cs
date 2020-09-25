5871fb2d3e93b7ace4f732efc250186742669943
mart610k <martin-baage@hotmail.com>
2020-09-25 09:06:24 +0200

    Added half dependency injection, wish to impleent it fully. added a few controllers and a bit of clean up

---
+++ WebAPI/WordGoalsAPI/WordGoalsAPI/Controllers/SensorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEndServices.Database;
+using BackEndServices.Sensor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordGoalsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SensorController : ControllerBase
+    {
+
+
+        WebDataActions dataActions;
+
+        public SensorController()
+        {
+            Dictionary<string, string> config = BackEndServices.ConfigReader.Config.ReadConfig(@"D:\Skole\H3\TemperatureSensorWorldGoals\mysqlConnectionConfig.cfg");
+
+
+            dataActions = new WebDataActions(new MySQLDatabaseAccess(config["host"], config["databasename"], config["username"], config["password"]));
+        }
+
+
+        [HttpGet("Sensors")]
+        public IEnumerable<ISensor> Get()
+        {
+            return dataActions.GetAllSensors();
+        }
+
+        [HttpGet("Sensor")]
+        public ISensor GetSensorByName([FromQuery(Name = "Name")] string page)
+        {
+            return dataActions.GetSensorByName(page);
+        }
+    }
+}
