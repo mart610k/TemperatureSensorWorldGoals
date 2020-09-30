6ba29234f4b7d7709df4ab4f4428fcb1e6d18c54
mart610k <martin-baage@hotmail.com>
2020-09-30 13:55:29 +0200

    Created API as a service

---
+++ Angular/world-goals-app/src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SimpleRoom } from './simple-room';
+import { Sensor } from './Sensor';
+import { Observable } from 'rxjs';
+import { SensorReading } from './sensor-reading';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseAPILink : String = "https://localhost:44379/api";
+
+  constructor(private http : HttpClient) { }
+
+
+  GetRooms() {
+    return this.http.get<SimpleRoom[]>(this.baseAPILink+"/Room/Rooms");
+  }
+
+  GetSensors(roomUUID : string){
+    return this.http.get<Sensor[]>(this.baseAPILink + "/Room/Sensors?Room="+roomUUID)
+  }
+
+  GetSensorReadings(roomUUID : string, sensorID : number,count :number ){
+    console.log("I am called");
+    return this.http.get<SensorReading[]>(this.baseAPILink + "/Room/Readings?Room=" + roomUUID + "&SensorID="+ sensorID +"&Count=" + count);
+  }
+}
