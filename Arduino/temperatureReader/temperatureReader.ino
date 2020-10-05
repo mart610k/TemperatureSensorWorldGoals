bbe38347078fa2515a9dc0fe029b3651cb9d8cd1
martin (darkmtbg2) <martin-baage@hotmail.com>
2020-10-05 13:15:06 +0200

    Added ethernet functionality to ardiuno reader

--- Arduino/temperatureReader/temperatureReader.ino
+++ Arduino/temperatureReader/temperatureReader.ino
@@ -1,35 +1,93 @@
 #include "DHT.h"
 
+#include <SPI.h>
+#include <Ethernet.h>
+
 #define DHTPIN 2
 
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+EthernetServer server(80);
+
 void setup() {
   Serial.begin(9600);
+
+  Ethernet.begin(mac, ip);
+  
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  
+  
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
   dht.begin();
 }
 
 
 void loop(){
-  
-  float h = dht.readHumidity();
-  
-  float t = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();
+          client.println("{");
+          client.println("\"temperature\":");
+          client.println(readTemperature());
+           client.println(",");
+          client.println("\"humidity\":");
+          client.println(readHumidity());
+          client.println("}");
+          
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
   }
-  
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print(t); 
-  Serial.println(" Celsius");
-  Serial.print(h);
-  Serial.println("% Humidity");
+}
 
-  delay(2000);
+float readHumidity(){
+  return dht.readHumidity();  
+}
+float readTemperature(){
+  return dht.readTemperature();
 }
